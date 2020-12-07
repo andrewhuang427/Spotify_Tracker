09fb9c2d83b955b337c5bcad2826b28dbc409d48
andrewhuang427 <andrewhuang@wustl.edu>
2020-12-06 20:54:40 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# CSE330
+464065
+
+# Rubric
+
+### Languages/Frameworks used (30 points)
+10 - Learned/Used React.JS Framework <br />
+10 - Learned/Used Express/Node.JS Framework <br />
+10 - Learned/Used Spotify API <br />
+
+### Functionality (60 points)
+10 - Users can login to application using personal Spotify Account to application <br />
+10 - Application displays user's list of top artists in the most recent month and all-time <br />
+10 - Application displays user's list of top played songs in the most recent month and all-time <br />
+10 - Users can play songs from application <br />
+10 - Application can view their playlists from the application <br />
+5 - Application is mobile friendly <br />
+5 - Application displays individual album covers and playlist covers <br />
+
+
+### Best Practices (5 points)
+3 - Code is readable and well formatted <br />
+2 - All pages pass the html validator <br />
+
+### Creative Portion (10 points)
+
+- Filter top songs by time
+- Used axios to perform fetch requests
---
+++ backend/server.js
@@ -0,0 +1,98 @@
+const axios = require("axios");
+const express = require("express");
+const path = require("path");
+const querystring = require("querystring");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const redirectURI = "http://localhost:8000/callback";
+const reactURI = "http://localhost:3000";
+
+const app = express();
+
+app.use(cors());
+
+let storedState = "";
+
+app.get("/login", function (req, res) {
+  const scopes = "user-read-private user-read-email user-top-read";
+  const stateString = generateRandomStateString(25);
+  storedState = stateString;
+
+  res.redirect(
+    `https://accounts.spotify.com/authorize?${querystring.stringify({
+      response_type: "code",
+      client_id: clientId,
+      scope: scopes,
+      redirect_uri: redirectURI,
+      state: stateString,
+    })}`
+  );
+});
+
+app.get("/callback", function (req, res) {
+  const code = req.query.code || null;
+  const state = req.query.state || null;
+
+  if (state === storedState) {
+    const requestBody = {
+      grant_type: "authorization_code",
+      redirect_uri: redirectURI,
+      code: code,
+    };
+
+    const config = {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${new Buffer.from(
+          `${clientId}:${clientSecret}`
+        ).toString("base64")}`,
+      },
+    };
+
+    axios
+      .post(
+        "https://accounts.spotify.com/api/token",
+        querystring.stringify(requestBody),
+        config
+      )
+      .then(function (response) {
+        const access_token = response.data.access_token;
+        const refresh_token = response.data.refresh_token;
+        res.redirect(
+          reactURI +
+            "/" +
+            querystring.stringify({
+              access_token: access_token,
+              refresh_token: refresh_token,
+            })
+        );
+      })
+      .catch(function (error) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      });
+  } else {
+    console.log("states keys did not match!");
+  }
+});
+
+const generateRandomStateString = function (length) {
+  let result = "";
+  let characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
+
+const PORT = 8000 || process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log("Server running on http://localhost:" + PORT);
+});
